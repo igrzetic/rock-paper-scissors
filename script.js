8959526abf9b7a3e16a34d602c098808af35a9fb
Ivan <130054944+igrzetic@users.noreply.github.com>
2024-02-08 08:55:24 +0100

    Projek dovršen, player i computer imaju array od kojega mogu birati tri elementa, za player-a postoji kontrola unosa, u 5 rundi se uspoređuje odabir iz tog polja i na kraju se prikazuje ukupan rezultat. Nadopunjen readme file sa detaljnim opisom projekta

---
+++ script.js
@@ -0,0 +1,55 @@
+function getPlayerChoice() {
+    let playerSelection = prompt("Choose (rock, paper, scissors):").toLowerCase();
+    const validPick = ['rock', 'paper', 'scissors'];
+    
+    while(!validPick.includes(playerSelection)) {
+        alert("Invalid choice. Pleas choose between rock, paper or scissors.");
+        playerSelection = prompt("Choose (rock, paper, scissors):").toLowerCase();
+    }
+    
+    console.log("Player: " + playerSelection);
+    return playerSelection;
+}
+
+function getComputerChoice () {
+    const randomPick = ['rock', 'paper', 'scissors'];
+    const randomNumber = Math.floor(Math.random() *3);
+    const choice = randomPick[randomNumber];
+    console.log("Computer: " + choice);
+    return choice;
+}
+
+function playRound() {
+    let round = 1;
+    let Pscore = 0;
+    let Cscore = 0;
+    const validPick = ['rock', 'paper', 'scissors'];
+
+
+    for(let i = 0; i<5; i++) {
+        console.log("Round: " + round);
+        const playerSelection = getPlayerChoice();
+        const computrSelection = getComputerChoice();
+
+        const playerIndex = validPick.indexOf(playerSelection);
+        const computerIndex = validPick.indexOf(computrSelection);
+
+        if(playerIndex === computerIndex) {
+            console.log("It's a tie!");
+        }
+        else if ((playerIndex + 1) % 3 === computerIndex) {
+            Cscore++;
+            console.log("Computer wins this round!")
+        }
+        else {
+            Pscore++;
+            console.log("Player wins this round!")
+        }
+        round++;
+    }
+
+    console.log("Final score: Player: " + Pscore + " Computer: " + Cscore);
+
+}
+
+playRound();
